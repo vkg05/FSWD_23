19c84f5b3f28f3861f1efe80cebf62e24a173ac6
Vishal Gupta <vkg051198@gmail.com>
2023-07-24 16:48:47 +0530

    javaScript -1

---
+++ JavaScript/L-1(Introduction to JS)/Basics of JavaScript/Topics/script.js
@@ -0,0 +1,19 @@
+// var age = 25;
+// console.log("Your age is "+age);
+// console.log(typeof age);
+
+// // type conversion
+
+// var x = "213";
+// const y = Number(x);
+
+// console.log(typeof y);
+ 
+
+let a = 23;
+let b = 24;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a>b);
+console.log(a<b);
