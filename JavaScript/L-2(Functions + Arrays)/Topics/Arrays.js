6096aa88056b46a1878dcb9c5ace202b69e20592
Vishal Gupta <vkg051198@gmail.com>
2023-07-25 16:12:16 +0530

    arrays

---
+++ JavaScript/L-2(Functions + Arrays)/Topics/Arrays.js
@@ -0,0 +1,35 @@
+// const words = ['hello', 'world', true, 123];
+// console.log(words);
+// console.log(words.length);
+
+
+// const arr1 = new Array('hello', 123, true);
+// console.log(arr1);
+// console.log(arr1.length);
+
+// const student = ['Ram','Vishal','Rahul','Sita'];
+
+// console.log(student);
+
+// student[4] = 'Lakshaman';
+// console.log(student[4]);
+// console.log(student.length);
+// console.log(student);
+
+
+// const student = ['vishal','harish','Ram','shyam',null];
+// console.log(student);
+// student[7] = 'hariom';
+// console.log(student[7]);
+// console.log(student[4]);
+// console.log(student);
+// console.log(student.length);
+
+
+
+const words = ['Ram', 'Vishal', 'Mohit', 'Sanjeev'];
+console.log(words);
+words.splice(2, 0, 'Sanjay');
+words.splice(3, 0, 'Vicky')
+console.log(words);
+
