6dab2d0330a0f1bb5fa207b77f1849afc4a92c8d
Vishal Gupta <vkg051198@gmail.com>
2023-08-22 16:56:35 +0530

    DOM navigation

---
+++ JavaScript/L-4(DOM Navigation)/navi.js
@@ -0,0 +1,14 @@
+const countingUl = document.getElementById("counting");
+
+const ulChildren = countingUl.parentNode;
+
+const ulParent = countingUL.parentNode;
+
+console.log(ulParent);
+
+console.log(ulChildren);
+
+const oneLi = document.getElementById("one");
+
+const siblingOneLi = oneLi.parentNode.childNodes;
+console.log(siblingOneLi); 
