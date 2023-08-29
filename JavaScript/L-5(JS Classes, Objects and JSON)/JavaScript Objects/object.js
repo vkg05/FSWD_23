16dd69da1e50875bce3fd1d14a367248daa96fa6
Vishal Gupta <vkg051198@gmail.com>
2023-08-29 11:05:52 +0530

    JS Objects

--- JavaScript/L-5(JS Classes, Objects and JSON)/JavaScript Objects/object.js
+++ JavaScript/L-5(JS Classes, Objects and JSON)/JavaScript Objects/object.js
@@ -20,11 +20,25 @@ for( let key in student){
     console.log(`${key} => ${student[key]} `);
 }
 
-// object destructring ES6
+function getVehicle() {
+    return {
+        type: 'Car',
+        wheels: 4
+    }
+}
+const myVehicle = getVehicle();
+console.log(myVehicle);
+console.log(typeof getVehicle);
+console.log(typeof myVehicle);
+
+
+
+// object destructring ES6 after 2015 javaScript
 
 const { name, age, hobbies } = student;
 console.log(name);
 console.log(age);
 console.log(hobbies);
 
- 
+
+// 
