a59d105f722fc164dcf34df0a568206099857bf6
Vishal Gupta <vkg051198@gmail.com>
2023-09-14 19:57:18 +0530

    JavaScript Modules

---
+++ JavaScript/L-6(Higher Order Functions and ES6 Concepts)/JavaScript Modules/server.js
@@ -0,0 +1,16 @@
+import { addition as add, subtraction, divide} from "./utilities.js";
+
+import multiply from "./utilities.js";
+
+const a = 5;
+const b = 4;
+
+const result = add(a, b);
+const sub = subtraction(a, b);
+const div = divide(a, b);
+
+console.log(result);
+console.log(sub);
+console.log(div);
+
+
